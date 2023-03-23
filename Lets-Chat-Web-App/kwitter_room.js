d3a14b9a87156b7a26f285a7e2a05a9838b82425
mightyfist <115439045+mightyfist@users.noreply.github.com>
2023-03-22 19:03:15 -0700

    Add files via upload

---
+++ Lets-Chat-Web-App/kwitter_room.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+firebaseConfig = {
+  apiKey: "AIzaSyDoL4Ci2qi6QCHLlfafpdMshlH24vSSpfs",
+  authDomain: "lets-chat-a3d0c.firebaseapp.com",
+  projectId: "lets-chat-a3d0c",
+  storageBucket: "lets-chat-a3d0c.appspot.com",
+  messagingSenderId: "27376545418",
+  appId: "1:27376545418:web:bb669f87ce27a5631c8773"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
